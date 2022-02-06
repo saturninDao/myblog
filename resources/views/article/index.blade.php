a407dbdf70becdc39c3406fa1443722f14b2401c
saturnin <saturnindao@gmail.com>
2022-02-06 03:08:00 +0000

    articles list in admin ok

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,1 +1,43 @@
-lol
+@extends('base')
+
+@section('content')
+
+<div class="mt-5 container">
+    <h1 class="mt-5 display-2">Liste des articles</h1>
+
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Titre</th>
+            <th scope="col">Sous-titre</th>
+            <th scope="col">Content</th>
+            <th scope="col">Création</th>
+            <th scope="col">Modification</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach( $articles as $article)
+            <tr>
+                <th scope="row">{{ $article->id }}</th>
+                <td>{{ $article->title }}</td>
+                <td>{{ $article->subtitle }}</td>
+                <td>{{ $article->content }}</td>
+                <td>{{ $article->created_at }}</td>
+                <td>{{ $article->updated_at }}</td>
+                <td>
+                    <button type="button" class="btn btn-warning">Modifier</button>
+                </td>
+                <td>
+                    <button type="button" class="btn btn-danger">Supprimer</button>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    <div class="d-flex justify-content-center">
+        {{ $articles->links() }}
+    </div>
+</div>
+
+@endsection
