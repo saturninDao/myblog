45b26f6bda609c2a638e54a9bf5152522ef670e0
saturnin <saturnindao@gmail.com>
2022-02-07 17:08:55 +0000

    testing models

---
+++ tests/Feature/ModelTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Faker\Factory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ModelTest extends TestCase
+{
+    public function testValidRegistration(){
+        $faker = Factory::create();
+        $email = $faker->unique()->email;
+        $count = User::count();
+
+        $this->post("/register",[
+            'email'=>$email,
+            'name'=>'test',
+            'password'=>'password',
+            'password_confirmation' => 'password'
+        ]);
+
+        $newCount = User::count();
+
+        $this->assertNotEquals($count,$newCount);
+
+    }
+}
